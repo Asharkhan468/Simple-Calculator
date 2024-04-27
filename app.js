0486298a31b7f7f780266f8875f27d12d939b3ae
Ashar Khan <asharullah.khan2468@gmail.com>
2024-04-27 23:49:02 +0500

    Calculator

---
+++ app.js
@@ -0,0 +1,45 @@
+let inputField = document.querySelector("#inputfield");
+
+function btn0() {
+  inputField.value = 0 ;
+}
+
+function btn1(){
+    inputField.value = 1;
+}
+
+function btn2() {
+  inputField.value = 2;
+}
+
+function btn3() {
+  inputField.value = 3;
+}
+
+function btn4() {
+ inputField.value = 4;
+}
+
+function btn5() {
+ inputField.value = 5;
+}
+
+function btn6() {
+inputField.value = 6;
+}
+
+function btn7() {
+  inputField.value = 7;
+}
+
+function btn8() {
+  inputField.value = 8;
+}
+
+function btn9() {
+  inputField.value = 9;
+}
+
+function clearBtn(){
+    inputField.value = "";
+}
