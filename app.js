8cb93ebfa24e40d50d6a759bd1e75e6676c8bf78
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-14 20:53:42 +0500

    Calculator

--- app.js
+++ app.js
@@ -1,69 +1,21 @@
-//Input  field
-
+let string = "";
+const buttons = document.querySelectorAll(".btn-mag");
 const input = document.querySelector("#input");
+input.value = string
 
-//Button click and render on input field
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // console.log(e.target.innerHTML);
+    input.value += `${e.target.innerHTML}`;
+    console.log(string);
 
-document.querySelector('#zero').addEventListener('click' , ()=>{
-    input.value+=0;
-})
-
-document.querySelector("#one").addEventListener('click' , ()=>{
-    input.value+=1;
-})
-
-document.querySelector('#two').addEventListener('click' , ()=>{
-    input.value+=2;
-})
-
-document.querySelector("#three").addEventListener('click' , ()=>{
-    input.value+=3;
-})
-
-document.querySelector('#four').addEventListener('click' , ()=>{
-    input.value+=4
-})
-
-document.querySelector("#five").addEventListener('click' , ()=>{
-    input.value+=5;
-})
-
-document.querySelector("#six").addEventListener('click' , ()=>{
-    input.value+=6;
-})
-
-document.querySelector("#seven").addEventListener('click' , ()=>{
-    input.value+=7;
-})
-
-document.querySelector("#eight").addEventListener('click' , ()=>{
-    input.value+=8;
-})
-
-document.querySelector("#nine").addEventListener('click' , ()=>{
-    input.value+=9;
-})
-
-document.querySelector("#equal").addEventListener('click' , ()=>{
-    console.log(input.value);
-})
-
-//Render clear btn
-
-
-document.querySelector('#clear').addEventListener('click' , ()=>{
-    input.value="";
-})
-
-
-//Rendering sign 
-
-
-document.querySelector('#add').addEventListener('click' , ()=>{
-    input.value='+'
-})
-
-
-
-
-
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    }else if (e.target.innerHTML == "C"){
+        string = "";
+        input.value = string;
+    } 
+    string = string + e.target.innerHTML;
+  });
+});
